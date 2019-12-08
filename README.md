81a53e21ec03af767309e01fe0763886ba681b30
Ramir Mesquita <Ramir_Mesquita@Dellteam.com>
2019-12-08 12:11:50 -0300

    Adding PM2 documentation, and changing index.js file to a regular express, withoud manual cluster, but keeping the old file for consulting

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # advanced-node
 Implementation of Advanced Node Concepts course using Multithreading, Clustering, Caching, Streaming and other cool concepts.
+
+## PM2
+
+To use (PM2)[pm2.keymetrics.io], first, install it globally:
+`npm i -g pm2`
+
+To start the application with an optimized cluster mode, use:
+`pm2 start index.js -i 0`
+
+To stop all instances, use:
+`pm2 delete index`
+
+To use PM2 monitoring tool:
+`pm2 monit`
--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require('cluster');
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-    // Cause index.js to be executed *again* but in child mode
-    cluster.fork();
-} else {
-    // I'm a child, I'm going to be executed like a server and nothing else
-    const express = require('express');
-    const crypto = require('crypto');
-    const app = express();
+// I'm a child, I'm going to be executed like a server and nothing else
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
 
-    app.get('/', (req, res) => {
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-            res.send('Hi there!');
-        });
+app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        res.send('Hi there!');
     });
+});
 
-    app.get('/fast', (req, res) => {
-        res.send('Really fast!');
-    });
+app.get('/fast', (req, res) => {
+    res.send('Really fast!');
+});
 
-    app.listen(3000);
-}
+app.listen(3000);
